115a5750eff238164e05cfcf95f2a4ced4994fd6
Adam Haris <iamadamharis@gmail.com>
2015-02-02 22:07:26 +0100

    rollback to standard boolean item until it is not updated

--- lib/formtastic-bootstrap/inputs/boolean_input.rb
+++ lib/formtastic-bootstrap/inputs/boolean_input.rb
@@ -5,10 +5,9 @@ module FormtasticBootstrap
       include Base
 
       def to_html
-        bootstrap_wrapping do
+        input_wrapping do
           hidden_field_html <<
-          "".html_safe <<
-          [label_with_nested_checkbox, hint_html].join("\n").html_safe
+          label_with_nested_checkbox
         end
       end
 
@@ -24,19 +23,67 @@ module FormtasticBootstrap
         )
       end
 
-      def checkbox_wrapping(&block)
-        template.content_tag(:div,
-          template.capture(&block).html_safe,
-          wrapper_html_options
-        )
+      def label_html_options
+        {
+          :for => input_html_options[:id],
+          :class => super[:class] - ['label'] # remove 'label' class
+        }
       end
 
-      def wrapper_html_options
-        super.tap do |options|
-          options[:class] = (options[:class].split - ["form-group"] + ["checkbox"]).join(" ")
+      def label_text_with_embedded_checkbox
+        check_box_html << "" << label_text
+      end
+
+      def check_box_html
+        template.check_box_tag("#{object_name}[#{method}]", checked_value, checked?, input_html_options)
+      end
+
+      def unchecked_value
+        options[:unchecked_value] || '0'
+      end
+
+      def checked_value
+        options[:checked_value] || '1'
+      end
+
+      def responds_to_global_required?
+        false
+      end
+
+      def input_html_options
+        {:name => input_html_options_name}.merge(super)
+      end
+
+      def input_html_options_name
+        if builder.options.key?(:index)
+          "#{object_name}[#{builder.options[:index]}][#{method}]"
+        else
+          "#{object_name}[#{method}]"
         end
       end
 
+      def checked?
+        object && boolean_checked?(object.send(method), checked_value)
+      end
+
+      private
+
+      def boolean_checked?(value, checked_value)
+        case value
+        when TrueClass, FalseClass
+          value
+        when NilClass
+          false
+        when Integer
+          value == checked_value.to_i
+        when String
+          value == checked_value
+        when Array
+          value.include?(checked_value)
+        else
+          value.to_i != 0
+        end
+      end
     end
   end
 end
